438555209f6349d91006d5f87d0b285215f564a7
422404 <elyanpoujol@gmail.com>
2020-10-30 19:02:21 +0100

    Defaults to tile layout

--- config.def.h
+++ config.def.h
@@ -44,9 +44,9 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "><>",      NULL },    /* first entry is default */
+	{ "[]=",      tile },    /* first entry is default */
                              /* no layout function means floating behavior */
-	{ "[]=",      tile }, 
+	{ "><>",      NULL }, 
 	{ "[M]",      monocle },
 };
 
@@ -81,8 +81,8 @@ static Key keys[] = {
 	{ WINDOWSKEY,                   XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
